dad768a5b06be7f95ca744e72fc6a41c20e0b990
JesusGTMB <136134499+JesusGTMB@users.noreply.github.com>
2023-08-12 14:40:26 +0000

    subiendo formulario

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+const { Resend } = require('resend');
+const fs = require('fs');
+require('dotenv').config();
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  fs.readFile('index.html', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error al leer el archivo');
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.post('/contact', async (req, res) => {
+  const { name, email, message } = req.body;
+  try {
+    await resend.emails.send({
+      from: 'Jesustorres.Dev <onboarding@resend.dev>',
+      to: ['jesusgtmb@gmail.com'],
+      subject: `Nuevo mensaje de ${name}`,
+      text: `Nombre: ${name}\nCorreo electrónico: ${email}\nMensaje: ${message}`,
+    });
+    res.send('Mensaje enviado con éxito');
+  } catch (error) {
+    res.send('Hubo un error al enviar el mensaje');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000');
+});
